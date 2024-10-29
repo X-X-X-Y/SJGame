a9ace747e456fa7e3a4fd6d5990175c262c62d32
XXXY_Laptop <790340163@qq.com>
2024-10-29 14:12:07 +0800

    [+]Add New Project

---
+++ Source/SJGame/SJGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SJGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SJGame, "SJGame" );
